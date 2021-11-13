59ff28feb0550688e917aa6a17db237314775d92
Masud8851 <masud1971.cse@gmail.com>
2021-11-13 20:36:08 +0600

    tesla client side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import AuthProvider from "./Context/AuthProvider";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Explore from "./Components/Explores/Explore";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import NotFound from "./Components/NotFound/NotFound";
+import Dashboard from "./Components/Dashboard/Dashboard/Dashboard";
+import Purchase from "./Components/Purchase/Purchase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<AuthProvider>
+				<Router>
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/home">
+							<Home />
+						</Route>
+						<PrivateRoute path="/explores">
+							<Explore />
+						</PrivateRoute>
+						<PrivateRoute path="/purchase/:id">
+							<Purchase />
+						</PrivateRoute>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/contact">
+							<Contact />
+						</Route>
+						<Route path="/dashboard">
+							<Dashboard />
+						</Route>
+						<Route path="/login">
+							<Login />
+						</Route>
+						<Route path="/register">
+							<Register />
+						</Route>
+						<Route path="*">
+							<NotFound />
+						</Route>
+					</Switch>
+				</Router>
+			</AuthProvider>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBB7v66a1pXH7COBrU8LWoxYaFf2n-GTaQ",
+	authDomain: "assignment-10-978e2.firebaseapp.com",
+	projectId: "assignment-10-978e2",
+	storageBucket: "assignment-10-978e2.appspot.com",
+	messagingSenderId: "130550921484",
+	appId: "1:130550921484:web:1b2687bd77479b9a547129",
+};
+
+export default firebaseConfig;
